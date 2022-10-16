90814de852b7386a1cdb18768c5a1d55c0805d3f
ctang415 <carrietang117@gmail.com>
2022-10-15 21:42:31 -0400

    Created basic layout for to-do list

--- src/index.js
+++ src/index.js
@@ -3,12 +3,29 @@ import './style.css';
 function createInterface() {
     const box = document.createElement('div')
     const headerText = document.createElement('div')
+    const projectInterface = document.createElement('div')
+    const homeInterface = document.createElement('li')
+    const todayInterface = document.createElement('li')
+    const weekInterface = document.createElement('li')
+    const importantInterface = document.createElement('li')
 
-    box.classList.add('flex')
+    headerText.classList.add('flex')
+    projectInterface.classList.add('grid')
+
 
     headerText.textContent = ['Taskit'];
+    homeInterface.textContent = ['Home']
+    todayInterface.textContent = ['Today']
+    weekInterface.textContent = ['This Week']
+    importantInterface.textContent = ['Important']
 
     box.appendChild(headerText)
+    box.appendChild(projectInterface)
+    projectInterface.appendChild(homeInterface)
+    projectInterface.appendChild(todayInterface)
+    projectInterface.appendChild(weekInterface)
+    projectInterface.appendChild(importantInterface)
+
 
     return box;
 }
