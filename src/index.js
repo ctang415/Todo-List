5dc3757851cbe743da35426df96330a065308bd0
ctang415 <carrietang117@gmail.com>
2022-10-15 22:17:39 -0400

    Updated top section of interface so they're evenly spaced

--- src/index.js
+++ src/index.js
@@ -4,13 +4,21 @@ function createInterface() {
     const box = document.createElement('div')
     const headerText = document.createElement('div')
     const projectInterface = document.createElement('div')
+    const topInterface = document.createElement('div')
     const homeInterface = document.createElement('li')
     const todayInterface = document.createElement('li')
     const weekInterface = document.createElement('li')
     const importantInterface = document.createElement('li')
+    const project = document.createElement('div')
+    const tasks = document.createElement('div')
+    const bottomInterface = document.createElement('div')
 
     headerText.classList.add('flex')
     projectInterface.classList.add('grid')
+    topInterface.classList.add('top')
+    bottomInterface.classList.add('bottom')
+    project.classList.add('project')
+    tasks.classList.add('tasks')
 
 
     headerText.textContent = ['Taskit'];
@@ -18,15 +26,24 @@ function createInterface() {
     todayInterface.textContent = ['Today']
     weekInterface.textContent = ['This Week']
     importantInterface.textContent = ['Important']
+    project.textContent = ['Projects']
+    tasks.textContent = ['All Tasks']
+
+    topInterface.appendChild(homeInterface)
+    topInterface.appendChild(todayInterface)
+    topInterface.appendChild(weekInterface)
+    topInterface.appendChild(importantInterface)
+
+    projectInterface.appendChild(topInterface)
+    projectInterface.appendChild(bottomInterface)
+
+    bottomInterface.appendChild(project)
+    bottomInterface.appendChild(tasks)
+
 
     box.appendChild(headerText)
     box.appendChild(projectInterface)
-    projectInterface.appendChild(homeInterface)
-    projectInterface.appendChild(todayInterface)
-    projectInterface.appendChild(weekInterface)
-    projectInterface.appendChild(importantInterface)
-
-
+    
     return box;
 }
 
